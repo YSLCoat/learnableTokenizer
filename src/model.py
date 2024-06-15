28f0bf78f80b08eec4632cc8a35c16cce63ea5ea
torstein.forseth <torstein.forseth@outlook.com>
2024-06-15 20:46:04 +0200

    remove sus code

--- src/model.py
+++ src/model.py
@@ -6,16 +6,11 @@ from timm.models._manipulate import checkpoint_seq
 from feature_extractors.spatial_transformer import AttentionSpatialTransformer
 from learnable_tokenizers.spixelFCN.models.Spixel_single_layer import SpixelNet
 
-
-
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-
-
 class differentiableSuperpixelEmbedding(nn.Module):
     def __init__(self, max_segments, n_channels=3, embed_dim=768):
         super().__init__()
-        self.superpixel_tokenizer = SpixelNet(num_segments=max_segments).to('cuda')
-        self.feature_extractor = AttentionSpatialTransformer(embed_dim, n_channels=n_channels).to('cuda')
+        self.superpixel_tokenizer = SpixelNet(num_segments=max_segments)
+        self.feature_extractor = AttentionSpatialTransformer(embed_dim, n_channels=n_channels)
         self.max_segments = max_segments  # This should be the maximum expected number of segments
 
     def forward(self, img):
@@ -49,7 +44,7 @@ class differentiableTokenizerVisionTransformer(nn.Module):
         super().__init__()
         
         
-        self.vit = timm.create_model(model_name, pretrained=pretrained, num_classes=num_classes).to(device)
+        self.vit = timm.create_model(model_name, pretrained=pretrained, num_classes=num_classes)
         
         self.embed_dim = self.vit.embed_dim
         
@@ -57,8 +52,7 @@ class differentiableTokenizerVisionTransformer(nn.Module):
             max_segments=max_segments,
             n_channels=num_channels,
             embed_dim=self.embed_dim
-        ).to(device)
-
+        )
         self.vit.patch_embed = self.patch_embed
         self.vit.pos_embed = nn.Parameter(torch.randn(1, max_segments + 1, self.embed_dim) * .02)
         self.vit.cls_token = nn.Parameter(torch.randn(1, 1, self.embed_dim)) 
