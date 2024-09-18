39ed39fcab0a3f2b453e84c831c7e7f1415a5adc
torstein.forseth <torstein.forseth@outlook.com>
2024-09-18 21:46:14 +0200

    nccl

--- src/train_utils.py
+++ src/train_utils.py
@@ -17,7 +17,7 @@ import quixdata
 def ddp_setup(rank, world_size):
     os.environ["MASTER_ADDR"] = "localhost"
     os.environ["MASTER_PORT"] = "12355"
-    init_process_group(backend="gloo", rank=rank, world_size=world_size)
+    init_process_group(backend="nccl", rank=rank, world_size=world_size)
     torch.cuda.set_device(rank)
 
 class Trainer:
