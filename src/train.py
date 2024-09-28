8f24214e38f7b251886e4a8b479053d5767ec842
torstein.forseth <torstein.forseth@outlook.com>
2024-09-28 18:09:15 +0200

    print gpus

--- src/train.py
+++ src/train.py
@@ -58,6 +58,18 @@ if __name__ == '__main__':
     parser.add_argument("--img_size", default=224, type=int)
     parser.add_argument("--n_segments", default=50, type=int)
     
+    # Check if CUDA is available
+    if torch.cuda.is_available():
+        # Get the number of GPUs
+        num_gpus = torch.cuda.device_count()
+        print(f"Number of available GPUs: {num_gpus}")
+        
+        # Print the name of each available GPU
+        for i in range(num_gpus):
+            print(f"GPU {i}: {torch.cuda.get_device_name(i)}")
+    else:
+        print("No GPUs available.")
+    
     args = parser.parse_args()
     world_size = torch.cuda.device_count()
     mp.spawn(main, args=(world_size, args), nprocs=world_size)
